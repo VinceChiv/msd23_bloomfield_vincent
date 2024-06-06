ef2b3174405df86ac2a6f766a1265993cb0a569f
Vincent Bloomfield <vincent.bloomfield@edu.fh-joanneum.at>
2024-06-06 10:50:05 +0200

    Added log4j dependancy; implemented in main + doc pics

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,13 +1,20 @@
 package at.fhj.msd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main
 {
+
+    private static Logger logger = LogManager.getLogger();
     public static void main(String[] args) {
 
         Calculator calc = new Calculator();
 
         System.out.println(calc.add(6,9));
         System.out.println(calc.minus(13, 4));
+        logger.info("This is informational");
+        logger.error("Yo this stuff erroin'");
         System.out.println(calc.multiply(5, 12));
         System.out.println(calc.divide(15,5));
         System.out.println("\nVincent Bloomfield");
